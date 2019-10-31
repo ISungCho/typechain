6f7b6a341608d85283d18c594da4ed5163a918cc
ISungCho <chois9660@gmail.com>
2019-10-31 16:52:49 +0900

    #5 Interface on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-// const name = "ISUNG",
-//   age = 24,
-//   gender = "female";
-
-const sayHi = (name: string, age: number, gender: string): string => {
-  return (`Hello ${name}, you are ${age}, you are a ${gender}`);
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
 }
 
-console.log(sayHi("ISUNG", 24, "female"));
+const person = {
+  name: "ISUNG",
+  age: 24,
+  gender: "female"
+}
+
+const sayHi = (person: Human): string => {
+  return (`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
+}
+
+console.log(sayHi(person));
 
 export { };
