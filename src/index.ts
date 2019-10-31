07155f02fef67d747889a4b5e54c66387ce4d163
ISungCho <chois9660@gmail.com>
2019-10-31 16:46:38 +0900

    #4 Types in Typescript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// const name = "ISUNG",
+//   age = 24,
+//   gender = "female";
+
+const sayHi = (name: string, age: number, gender: string): string => {
+  return (`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+console.log(sayHi("ISUNG", 24, "female"));
+
+export { };
