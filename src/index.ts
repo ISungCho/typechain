db47a0f4cd075bcd59a52d237753e64336bad254
ISungCho <chois9660@gmail.com>
2019-11-01 14:26:48 +0900

    #7 Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,28 @@
-// interface Human {
-//   name: string;
-//   age: number;
-//   gender: string;
-// }
-
-// const person = {
-//   name: "ISUNG",
-//   age: 24,
-//   gender: "female"
-// }
-
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  // 클래스가 시작할 때마다 호출
-  // 클래스로부터 객체(object)를 만들 때마다
-  constructor(name: string, age: number, gender: string){ 
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number,
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const lynn = new Human("Lynn", 18, "female")
+const genesisBlock: Block = new Block(0, "20202022020", "", "Hello", 123456);
 
-const sayHi = (person: Human): string => {
-  return (`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
-}
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(lynn));
+console.log(blockchain)
 
 export { };
